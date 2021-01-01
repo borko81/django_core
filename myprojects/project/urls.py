743af815052fad7953bac1f500c1cc8a70fa464b
BorisSt <bstoilov81@gmail.com>
2021-01-01 18:17:58 +0200

    Add from linux

--- myprojects/project/urls.py
+++ myprojects/project/urls.py
@@ -3,8 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app01/', include('app01.urls')),
-    path('app02/', include('app02.urls')),
-    path('app03/', include('app03.urls')),
     path('mandl/', include('message_and_list.urls')),
 ]
